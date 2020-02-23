84edf30ccab4fdf5a46123734edc3bf1d44ff6ab
Fiifi-Jr <42737537+arkhurst@users.noreply.github.com>
2020-02-23 15:06:11 +0000

    Update mergeSort.cpp

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,41 +1,35 @@
 //ID:10665476
 
-#include <bits/stdc++.h> 
-using namespace std; 
-//implementing merge sort 
+//implementing merge sort in c++
+#include <iostream>;
+using namespace std;
 
-void mergeSort ( int arr[], int lower, int higher) {
-	if(lower >= higher){
-		return ;
-	}else{
-		int mid = (lower + higher)/2;
-		mergeSort(arr, lower, mid);
-		mergeSort(arr, mid + 1, higher);
-		merge(arr, lower, mid, mid+1, higher);
-	}
-}
 
-void merge( int arr [], int leftLowerValue, int leftHigherValue, int rightLowerValue, int rightHigherValue){
+int merge( int arr [], int leftLowerValue, int leftHigherValue, int rightLowerValue, int rightHigherValue){
 	int length = rightHigherValue - leftLowerValue + 1 ;
 	int temp[length];
 	int left = leftLowerValue;
 	int right = rightLowerValue;
 	
-	for (int i = 0, i < length; i++){
-		if(left > leftHigherValue)
-		     temp[i] = arr[right++]
-		else if( right > rightHigherValue)
+	for (int i = 0; i < length; i++){
+		if(left > leftHigherValue){
+			temp[i] = arr[right++];
+		}else if ( right > rightHigherValue){
+		
 		     temp[i] = arr[left++];
-		else 
-		  temp[i] = arr[right ++]	      
+		}
+		else {
+			  temp[i] = arr[right ++];	
+		}
+		      
 	}
 	
 
-	for (int i=0, i<length; i++)
-	   a[leftLowerValue] = temp[i];
+	for (int i=0; i<length; i++)
+	   arr[leftLowerValue] = temp[i];
 }
 
-  void printArray (int arr[], int length){
+  int printArray (int arr[], int length){
     int i;
     for (i=0; i < length; i++)
     {
@@ -44,18 +38,29 @@ void merge( int arr [], int leftLowerValue, int leftHigherValue, int rightLowerV
     printf("\n");
 
   }	
+  
+  int mergeSort ( int arr[], int lower, int higher) {
+	if(lower >= higher){
+		return 0 ;
+	}else{
+		int mid = (lower + higher)/2;
+		mergeSort(arr, lower, mid);
+		mergeSort(arr, mid + 1, higher);
+		merge(arr, lower, mid, mid+1, higher);
+	}
+}
 
 //taking a given value of arrays  int arr[] = {32, 45, 89, 5, 47};
 int main(){
-	 int arr[] = {32, 45, 89, 5, 47};
-	    
+	 int array[] = {1,3,5,8,2,4};
+	 int length = 6  ;
   
    printf("Given array: \n");
-   printArray(arr, length);
+   printArray(array,length);
     
-   mergeSort(arr, 0, length - 1)
+   mergeSort(array, 0, length - 1);
      
     printf("\nSorted array: \n");
-    printArray(arr, length);
+    printArray(array, length);
     return 0;
 }
